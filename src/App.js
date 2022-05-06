ec545ce7ce780a6d7d541c263280bbb385b4a74b
yushika99 <it20105952@my.sliit.lk>
2022-05-06 15:23:17 +0530

    front css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import Reactfrom from 'react';
+import Task from './Tasks'
+import{paper,TextField}from '@material-ui/core';
 import './App.css';
+import Tasks from './Tasks';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Tasks {
+  state={tasks:[],currentTask:""}
+  render(){
+    return(
+    <div className= "App flex">
+      <paper elevation ={3}>
+
+      </paper>
+      
+    </div> );
+  }
 }
 
 export default App;
